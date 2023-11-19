67a7b34ac922dddd3e2b68e452c7d90de906af9a
pairing4good <pairing4good@gmail.com>
2023-11-19 06:28:31 -0600

    show logged in username

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
   const [data, setData] = useState('');
+  const [user, setUser] = useState('');
 
   useEffect(() => {
     (async function () {
       const { text } = await( await fetch(`/api/message`)).json();
       setData(text);
     })();
+
+    (async function () {
+      const { text } = await( await fetch(`/.auth/me`)).json();
+      setUser(text);
+    })();
   });
 
-  return <div>{data}</div>;
+  return <div>{data} from {data.clientPrincipal.userDetails.username}</div>;
 }
 
 export default App;
